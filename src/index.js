f4d847a699734906051cf831862980023f6005eb
menor <josemenor@gmail.com>
2016-08-07 09:58:54 +0200

    Switch to jsx syntax Start preparing the view to render a random song

--- src/index.js
+++ src/index.js
@@ -1,50 +1,87 @@
 // Get data for spotify for one year
 // play the song
 // hook the changes in the value to the changes in the song
-
-import Cycle from '@cycle/core'
-import {div, h2, input, makeDOMDriver} from '@cycle/dom'
+// import xs from 'xstream'
+import Cycle from '@cycle/rx-run'
+import {makeDOMDriver} from '@cycle/dom'
 import {makeHTTPDriver} from '@cycle/http'
+import {html} from 'snabbdom-jsx'
+
+// DOM source: slider value
+// HTTP sink: request sent
+// HTTP source: response received
+// DOM sink: data displayed
+
+const renderSlider = (value) =>
+  <div>
+    <h2>Year: {value}</h2>
+    <input
+      id='slider'
+      type='range'
+      min='1920'
+      max='2016'
+      value={value}
+    />
+  </div>
+
+const renderSong = (song) =>
+  <div className='song-details'>
+    <h2 className='song-name'>{ song.name }</h2>
+    <h1 className='song-artist'>{ song.artist }</h1>
+    <h3 className='song-album'>{ song.album }</h3>
+  </div>
 
 function main (sources) {
-  const changeValue$ = sources.DOM.select('#slider')
+  const initialState = {
+    year: 1977
+  }
+
+  // year$ is a stream of changes in the slider
+  const year$ = sources.DOM.select('#slider')
     .events('input')
     .map(ev => ev.target.value)
 
-  const DEFAULT_VALUE = 1977
-  const state$ = changeValue$.startWith(DEFAULT_VALUE)
+  // We compute this based on year$ to send the request
+  const requestSong$ = year$
+    .map(year => {
+      return {
+        url: `https://api.spotify.com/v1/search?q=year%3A${year}&type=track`,
+        method: 'GET',
+        Accept: 'application/json',
+        Authorization: 'Bearer BQA8On_pUfgmdAGr-DrW2Rc_gvtF8cnFaAarNJ33q2C-QoWW9AHH6Ux6wyWBn6IW2-oamPwe-BH0f_pGKya_lwxTSK3n6g3QD28GdcP6-1pggk0lZJA9JGT7EdnKiEghDezdnGyuObzwvYJ0e_QYIQ'
+      }
+    })
 
-  const vdom$ = [range$, songPlayer$]
+  // We handle the response here (source)
+  // response$$ means it's a stream that emits response streams
+  // ----------r-----------------r-------------------------r------
+  // ----------\--a              \--b---b-b                \--c
+  // const response$$ = sources.HTTP
+  //
+  // const response$ = response$$.switch()
+  // const song$ = response$.map(response => response.body)
 
-  const range$ = state$.map(value =>
-    div([
-      h2('Year: ' + value),
-      input('#slider', {
-        type: 'range', min: 1920, max: 2016, value
-      })
-    ])
-  )
+  // We derive the state from the year but still need to incorporate
+  // the songsRequest$
+  const state$ = year$.startWith(initialState.year)
 
-  const getSong$ = state$.map((year) => {
-    return {
-      url: `https://api.spotify.com/v1/search?q=year%3A${year}&type=track`,
-      method: 'GET',
-      Accept: 'application/json',
-      Authorization: 'Bearer BQA8On_pUfgmdAGr-DrW2Rc_gvtF8cnFaAarNJ33q2C-QoWW9AHH6Ux6wyWBn6IW2-oamPwe-BH0f_pGKya_lwxTSK3n6g3QD28GdcP6-1pggk0lZJA9JGT7EdnKiEghDezdnGyuObzwvYJ0e_QYIQ'
-    }
-  })
+  const testSong = {
+    'name': 'Dame Dame',
+    'artist': 'Goyo Ramos',
+    'album': 'Donde estara el dinero de Rafael'
+  }
 
-  const song$ = sources.HTTP.select('songs')
-    .map(res => res.body)
-    .startWith(null)
-
-  const songPlayer$ = song$.map(song =>
-    div(song)
+  const vdom$ = state$.map(value =>
+    <div>
+      { renderSlider(value) }
+      <hr/>
+      { renderSong(testSong) }
+    </div>
   )
 
   const sinks = {
     DOM: vdom$,
-    HTTP: getSong$
+    HTTP: requestSong$
   }
 
   return sinks
